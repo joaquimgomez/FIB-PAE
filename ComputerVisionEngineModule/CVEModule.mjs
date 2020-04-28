20dece152b346b14ceba1e718ce1f7e1ac7ee2ed
Joaquim Gómez <9joaquimgomez@gmail.com>
2020-04-28 21:51:27 +0200

    [ID-0008] IconsProblemSolver debugged and working

--- ComputerVisionEngineModule/CVEModule.mjs
+++ ComputerVisionEngineModule/CVEModule.mjs
@@ -12,6 +12,215 @@ const MINIMUM_AREA_FACTOR = 1/10;	// TODO: Doc
 
 
 // --------------- CLASSES AREA ---------------
+/**
+ * 
+ */
+class ComputerVisionEngineProblemSolver {
+	// --------------- CLASS FIELDS AREA ---------------
+	problemImage;			// Subimage of the problem
+	toShow_problemImage;	//
+
+
+	// --------------- CLASS FUNCTIONS AREA ---------------
+
+	/**
+	 * Precondition: The subimage (i.e. subproblem) is a valid area in gray scale color model.
+	 */
+	constructor(subimage) {
+		this.problemImage = subimage;
+		this.toShow_problemImage = subimage;
+	}
+
+	/**
+	 * 
+	 */
+	solve() {
+		// Nothing to solve in a superclass.
+	}
+}
+
+/**
+ * 
+ */
+class TextProblemSolver extends ComputerVisionEngineProblemSolver {
+	// --------------- CLASS FIELDS AREA ---------------
+
+
+	// --------------- CLASS FUNCTIONS AREA ---------------
+	
+	/**
+	 * 
+	 */
+	solve() {
+	
+	}
+	
+}
+
+/**
+ * 
+ */
+class CheckboxesProblemSolver extends ComputerVisionEngineProblemSolver {
+	// --------------- CLASS FIELDS AREA ---------------
+
+	
+	// --------------- CLASS FUNCTIONS AREA ---------------
+
+	/**
+	 * 
+	 */
+	isABox(contour) {
+		let approx = new cv.Mat();
+
+		// Get polygon
+		cv.approxPolyDP(contour, approx, 0.01*cv.arcLength(contour, true), true);
+
+		return (approx.size().height == 4);
+	}
+
+	/**
+	 * 
+	 */
+	solve() {
+		
+		// Obtain contours
+
+		// Extract boxes from contours
+
+		// Boxes depuration
+
+		// Checked box detection
+	
+	}
+}
+
+
+/**
+ * 
+ */
+class IconsProblemSolver extends ComputerVisionEngineProblemSolver {
+	// --------------- CLASS FIELDS AREA ---------------
+
+	numberDetectedCirles = 0;							//
+	positionsAndRadiuDetectedCircles = new Array();		// (x, y, r) of every cirle detected
+	radiusExpansionFactor = 0.15;						//
+	thres = 200;										//
+	corners = new Array();
+
+	
+	// --------------- CLASS FUNCTIONS AREA ---------------
+
+	/**
+	 * 
+	 * @param detectedCircles 
+	 */
+	constructCirclesEstructure(detectedCircles) {
+		// Number of detected circles
+		this.numberDetectedCirles = detectedCircles.cols;
+
+		// Data extraction and formatting
+		for (let i = 0; i < this.numberDetectedCirles; ++i) {
+			let x = detectedCircles.data32F[i * 3];
+			let y = detectedCircles.data32F[i * 3 + 1];
+			let r = detectedCircles.data32F[i * 3 + 2];
+
+			// Save point inside the structure;
+			this.positionsAndRadiuDetectedCircles.push(new Array(x, y, r));
+		}
+
+		// Sort per x
+		this.positionsAndRadiuDetectedCircles = this.positionsAndRadiuDetectedCircles.sort((a, b) => { 
+			return a[0] - b[0]; 
+		});
+	}
+
+	/**
+	 * 
+	 */
+	cornersDetection() {
+		// Corners detection
+		let corners = new cv.Mat();
+		cv.cornerHarris(this.problemImage, corners, 2, 3, 0.04);
+
+		// Corners normalization
+		let corners_norm = new cv.Mat();
+		cv.normalize(corners, corners_norm, 0, 255, cv.NORM_MINMAX);
+
+		// Scaling corners
+		let corners_norm_scaled = new cv.Mat();
+		cv.convertScaleAbs(corners_norm, corners_norm_scaled);
+
+		// Corners extraction based on a thres
+		iteratingCornersX: for (let i = 0; i < corners_norm_scaled.rows; ++i) {
+			iteratingCornersY: for (let j = 0; j < corners_norm_scaled.cols; ++j) {
+				let value = corners_norm_scaled.data[i * corners_norm_scaled.cols * corners_norm_scaled.channels() + j * corners_norm_scaled.channels()];
+
+				if (value >= this.thres) {
+					// Corners contains a list of pairs (x,y)
+					this.corners.push(new Array(j,i));
+				}
+			}
+		}
+	}
+	
+
+	/**
+	 * 
+	 */
+	solve() {
+		// Circle detection
+		let circles = new cv.Mat();
+		cv.HoughCircles(this.problemImage, circles, cv.HOUGH_GRADIENT, 1.5, 200);//, minRadius=0, maxRadius=0);
+
+		// Circle estructure construction sorted (at this.positionsAndRadiuDetectedCircles)
+		this.constructCirclesEstructure(circles);
+
+		// Corners detection (at this.corners)
+		this.cornersDetection();
+
+		// Counting corners inside expanded circles
+		let cornersPerCircle = new Array(this.numberDetectedCirles);
+		cornersPerCircle.fill(0);
+
+		iteratingCorners: for (let i = 0; i < this.corners.length; ++i) {
+			let xCorner = this.corners[i][0];
+			let yCorner = this.corners[i][1];
+
+			iteratingCircles: for (let j = 0; j < this.positionsAndRadiuDetectedCircles.length; ++j) {
+				let xCircle = this.positionsAndRadiuDetectedCircles[j][0];
+				let yCircle = this.positionsAndRadiuDetectedCircles[j][1];
+				let expandedCircleRadius = this.positionsAndRadiuDetectedCircles[j][2];
+				expandedCircleRadius = expandedCircleRadius + (expandedCircleRadius*0.15 | 0);
+
+				let xCircleMinusR = xCircle - expandedCircleRadius;
+				let xCirclePlusR = xCircle + expandedCircleRadius;
+				let yCircleMinusR = yCircle - expandedCircleRadius;
+				let yCirclusPlusR = yCircle + expandedCircleRadius;
+
+				if (xCorner >= xCircleMinusR && xCorner <= xCirclePlusR &&
+					yCorner >= yCircleMinusR && yCorner <= yCirclusPlusR) {
+					++cornersPerCircle[j];
+					break iteratingCircles;
+				}
+			}
+		}
+
+		// Solution generation and return
+		// Solution based on the following ordering: Happy, Meh, Sad
+		let indexOfMaxCountPerCircle = cornersPerCircle.indexOf(Math.max(...cornersPerCircle));
+
+		if (indexOfMaxCountPerCircle == 0) {
+			return 'Happy';
+		} else if (indexOfMaxCountPerCircle == 1) {
+			return 'Meh';
+		} else if (indexOfMaxCountPerCircle == 2) {
+			return 'Sad';
+		} else {
+			return 'None';
+		}
+	}
+
+}
 
 
 /**
@@ -35,8 +244,8 @@ class ComputerVisionEngine {
 	constructor(image) {
 		try {
 			this.image = cv.imread(image);
-			this.grayScale_image = this.image
-			this.toShow_image = this.image;
+			this.grayScale_image = this.image.clone();
+			this.toShow_image = this.image.clone();
 
 			// TODO: recibir los tipos de las preguntas
 
@@ -126,13 +335,16 @@ class ComputerVisionEngine {
 	
 	*/
 	subproblemsTreatment(subproblems) {
-		for (var i = 0; i < length(subproblems); i++) {
+		for (var i = 0; i < subproblems.length; i++) {
 			// TODO: Definir forma de transmisión de la tipología de preguntas.
-			if (responsesTypes[i] == 'text') {
-				// TODO: Tratamiento de las respuestas de tipo texto
-			} else if (responsesTypes[i] == 'icons_problem') {
-				// TODO: Tratamiento de las respuestas de tipo texto
-			}
+			/*if (responsesTypes[i] == 'text') {
+				let textProblemSolver = new TextProblemSolver(subproblems[i]);
+				let text = textProblemSolver.solve();
+			} else if (responsesTypes[i] == 'icons_problem') {*/
+				let iconsProblemSolver = new IconsProblemSolver(subproblems[i]);
+				let response = iconsProblemSolver.solve();
+				console.log(response);
+			//}
 		}
 	}
 
@@ -154,11 +366,13 @@ class ComputerVisionEngine {
 
 			//
 			let subproblems = this.generateSubProblemImages(responseAreas);
-
-			return this.toShow_image;
+			this.toShow_image = subproblems[5];
 
 			//
-			//results = this.subproblemsTreatment(subproblems);
+			let results = this.subproblemsTreatment([subproblems[5]]);
+			
+			//return results;
+			return this.toShow_image;
 
 		} else {
 			console.log("Non-debug mode not available.")
