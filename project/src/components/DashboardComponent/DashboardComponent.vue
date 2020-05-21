22050b9f4a7ddd970a59611cf3ed5e5e99a2e007
tataslas <tataslas@gmail.com>
2020-05-21 19:40:42 +0200

    [ID-0038] Created component dashboard with 3 buttons

---
+++ project/src/components/DashboardComponent/DashboardComponent.vue
@@ -0,0 +1,40 @@
+<script>
+/**
+ * Import Vue and Axios for request api intranet
+ */
+import Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import Vuex from 'vuex'
+//import HeaderComponent from '@/components/HeaderComponent/HeaderComponent'
+import QuestionnaireComponent from '@/components/QuestionnaireComponent/QuestionnaireComponent'
+import { mapState } from 'vuex';
+
+Vue.use(VueAxios, axios)
+Vue.use(Vuex)
+
+/**
+ * Export component Main, the father component in the aplication
+ */
+export default {
+  name: 'DashboardComponent',
+  data () {
+      return {
+      }
+  },
+  computed: {
+    ...mapState([
+    ])
+  },
+  async mounted(){
+  },
+
+  components:{    
+    QuestionnaireComponent
+  }
+
+}
+</script>
+
+<template src="./DashboardComponent.html"></template>    
+<style src="./DashboardComponent.css"></style>
