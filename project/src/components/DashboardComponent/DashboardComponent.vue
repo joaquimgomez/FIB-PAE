6538153f9b320f3f3dc56ba0fb37bea68278588b
tataslas <tataslas@gmail.com>
2020-06-01 21:53:44 +0200

    [ID-0040] empty PopUp

--- project/src/components/DashboardComponent/DashboardComponent.vue
+++ project/src/components/DashboardComponent/DashboardComponent.vue
@@ -9,6 +9,8 @@ import Vuex from 'vuex'
 //import HeaderComponent from '@/components/HeaderComponent/HeaderComponent'
 import QuestionnaireComponent from '@/components/QuestionnaireComponent/QuestionnaireComponent'
 import { mapState } from 'vuex';
+import "element-ui/lib/theme-chalk/index.css";
+
 
 Vue.use(VueAxios, axios)
 Vue.use(Vuex)
@@ -20,13 +22,65 @@ export default {
   name: 'DashboardComponent',
   data () {
       return {
+        showDialog: true,
+        tableData: [],
+        columns: [
+        { 
+          label: "Name questionnaire", 
+          width: 60, 
+          sortable: true,
+          prop: "name" 
+        },
+        { 
+          label: "Organization", 
+          width: 80, 
+          sortable: true,
+          prop: "organization" 
+        },
+        {
+          label: "Date", 
+          width: 100,
+          prop: "date",
+          sortable: true
+        }
+        ],
+        loading: false
       }
   },
   computed: {
     ...mapState([
     ])
   },
-  async mounted(){
+  methods:{
+    onSortChange(order) {
+      this.tableData = this.tableData.sort(function(a, b) {
+        if (order.order == "descending") return b[order.prop] - a[order.prop];
+        else return a[order.prop] - b[order.prop];
+      });
+    }
+  },
+  mounted(){
+    var self = this;
+    axios.get(
+        "http://localhost:3000/poll"
+      )
+      .then(response => {
+        var data = response.data;
+        console.log("response data: ", data);
+
+        data.forEach(q => {
+          self.tableData.push({
+            id: q.id,
+            name: q.name,
+            organization: q.org_name,
+            date: q.created_at
+          })
+        });
+      })
+      .catch(error => {
+        this.launchNotify("Error", "Error al hacer get all de las encuestas", "error");
+        console.log(error);
+      });
   },
 
   components:{    
--- project/src/router.js
+++ project/src/router.js
@@ -24,7 +24,7 @@ export default new Router({
     // },
     {
       path: "/app",
-      name: "dasboard",
+      name: "dashboard",
       component: () => import("./components/DashboardComponent/DashboardComponent")
     },
     // {
