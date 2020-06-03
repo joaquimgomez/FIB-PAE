90f5c36cbd7df9066a29eb27017ea37f213b48fb
paltet <pau.altet.pujol@hotmail.com>
2020-06-03 21:51:29 +0200

    [ID-0039] Initial page

---
+++ project/src/components/NewQuestionnaireComponent/NewQuestionnaireComponent.vue
@@ -0,0 +1,14 @@
+<script>
+
+//import Vue from 'vue'
+//import axios from 'axios'
+//import VueAxios from 'vue-axios'
+//import Vuex from 'vuex'
+
+export default {
+    name: 'newquestionnaire'
+}
+</script>
+
+<template src="./NewQuestionnaireComponent.html"></template>
+<style src="./NewQuestionnaireComponent.css"></style>
