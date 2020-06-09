a43e09cc3451c3fffb8741d0591ec02168eb70c6
paltet <pau.altet.pujol@hotmail.com>
2020-06-09 12:13:46 +0200

    [ID-0039] Basic New Questionnaire

--- project/src/components/NewQuestionnaireComponent/NewQuestionnaireComponent.vue
+++ project/src/components/NewQuestionnaireComponent/NewQuestionnaireComponent.vue
@@ -1,12 +1,25 @@
 <script>
 
-//import Vue from 'vue'
+//import Vue from 'vue';
 //import axios from 'axios'
 //import VueAxios from 'vue-axios'
 //import Vuex from 'vuex'
 
+import questionform from './QuestionFormComponent.vue'
+
 export default {
-    name: 'newquestionnaire'
+    name: 'newquestionnaire',
+    data() {
+        return {
+            num: 1,
+            name: ""
+        }
+    },
+    methods: {
+    },
+    components: {
+        questionform
+    }
 }
 </script>
 
---
+++ project/src/components/NewQuestionnaireComponent/QuestionFormComponent.vue
@@ -0,0 +1,52 @@
+<template>
+    <el-card style="background-color: #C0B8F5;">
+        <el-form>
+            <el-row>
+                <el-col :span="12">Question {{number}}</el-col>
+                <el-col :span="12"><select v-model="type">
+                            <option disabled value="">Select a type</option>
+                            <option>Regular</option>
+                            <option>Checkbox</option>
+                        </select>
+                </el-col>
+            </el-row>
+            <br>
+            <el-row>
+                <el-input v-model="body" placeholder="Question body"></el-input>
+            </el-row>
+            <div v-if="type === 'Checkbox'">
+                <br>
+                <el-row>
+                    <el-col :span="12">Number of options</el-col>
+                    <el-col :span="12"><el-input-number v-model="numanswers" :min="2"></el-input-number></el-col>
+                </el-row>
+                <div v-for="n in numanswers" v-bind:key="n">
+                    <el-row>
+                        <br>
+                        <el-col :span="8">Option {{n}}</el-col>
+                        <el-col :span="16"><el-input v-model="answers[n-1]"></el-input></el-col>
+                    </el-row>
+                </div>
+            </div>
+        </el-form>
+    </el-card>
+</template>
+
+<script>
+    export default{
+        name:'questionform',
+        props: {
+            number: Number
+        },
+        data(){
+            return {
+                type: "",
+                body: "",
+                numanswers: 2,
+                answers: []
+            }
+        },
+        methods: {
+        }
+    }
+</script>
