ca5fa54422ba77cb7cbee161ba17ba3ef0b81299
tataslas <tataslas@gmail.com>
2020-06-14 13:04:12 +0200

    [ID-0116] el-search-table-pagination in english

--- project/src/components/StatsComponent/StatsComponent.vue
+++ project/src/components/StatsComponent/StatsComponent.vue
@@ -4,21 +4,69 @@ import axios from 'axios'
 import VueAxios from 'vue-axios'
 import Vuex from 'vuex'
 import { mapState } from 'vuex';
+import ElSearchTablePagination from "el-search-table-pagination";
 
-Vue.use(VueAxios, axios)
-Vue.use(Vuex)
+Vue.use(VueAxios, axios);
+Vue.use(Vuex);
+Vue.use(ElSearchTablePagination);
 
 export default {
   name: 'StatsComponent',
   data () {
       return {
+        tableData: [],
+        columns: [
+          {
+            prop: "question", 
+            label: "Pregunta", 
+            width: 400
+          },
+          { 
+            label: "Respuesta", 
+            minwidth: 50, 
+            sortable: true,
+            slotName: "answer" ,
+            show: false
+          },
+          {
+            label: "Problema", 
+            width: 200,
+            slotName: "problem",
+            required: true
+          },
+          {
+            label: "Medida", 
+            width: 200,
+            slotName: "measure"
+          },
+          {
+            label: "Comentario", 
+            width: 200,
+            slotName: "comment"
+          }
+        ],
       }
   },
   computed: {
     ...mapState([
     ])
   },
-  async mounted(){
+  methods: {
+    launchNotify(title, message, type) {
+      this.$notify({
+        title: title,
+        message: message,
+        type: type
+      });
+    },
+    onSortChange(order) {
+      this.tableData = this.tableData.sort(function(a, b) {
+        if (order.order == "descending") return b[order.prop] - a[order.prop];
+        else return a[order.prop] - b[order.prop];
+      });
+    },
+  },
+  mounted(){
   },
 
   components:{ }
