abc325af917eb7b136a0ef88536bfb773ef05202
Joaquim Gómez <9joaquimgomez@gmail.com>
2020-06-14 16:38:32 +0200

    [ID-0037] Module working

--- project/src/ComputerVisionEngineModule/configure.sh
+++ project/src/ComputerVisionEngineModule/configure.sh
@@ -1,18 +1,20 @@
-# Requirement: OpenCV4 must be installed on the system
-
-# Preventing the auto build script
-export OPENCV4NODEJS_DISABLE_AUTOBUILD=1
-
-# opencv4nodejs library
-npm install --save opencv4nodejs@^5.2.0
+# Requirement:
+# - OpenCV4 must be installed on the system
+# - There should be no spaces in the path to the project folder.
 
 # electron & electron-rebuild
 npm install electron --save-dev
 npm install --save-dev electron-rebuild
 
+# Vision API of Google Cloud Platform
+npm install @google-cloud/vision
+
+# Preventing the auto build script
+export OPENCV4NODEJS_DISABLE_AUTOBUILD=1
+
+# opencv4nodejs library
+npm install --save opencv4nodejs@5.2.0
+
 # At this point the package.json should has the script:
 # "electron-rebuild": "electron-rebuild -w opencv4nodejs"
 npm run electron-rebuild
-
-# Vision API of Google Cloud Platform
-npm install @google-cloud/vision
--- project/src/controllers/computerVisionEngineController.js
+++ project/src/controllers/computerVisionEngineController.js
@@ -1,38 +1,26 @@
-const fs = require('fs');
 //const cv = require('opencv4nodejs');
-//const CVE = require('../ComputerVisionEngineModule/CVEngine.js');
+const CVE = require('../ComputerVisionEngineModule/CVEngine.js');
 
 
 exports.post = function(req, res, next) {
     if(req.body.file) {
         try {
             // Engine creation and image resolution
-            console.log("Image: ", req.body.file);
-            console.log("Types: ", req.body.expected_data)
-            //let cve = new CVE(req.file, req.expected_data);
-            // let results = cve.run();
+            let cve = new CVE(req.body.file, req.body.expected_data);
+            let results = cve.run();
 
-            // // Waiting for the promises
-            // Promise.all(results).then(values => {
-            //     //res.json(req.file);
-            //     console.log(values);
-
-            //     // Sending results
-
-            //     // Deleting received image
-            //     //fs.unlinkSync('../ComputerVisionEngineModule/tmp/uploads/' + req.file.filename);
-            // })
-
-
-            //TODO
-            res.status(200).send({
-                message: req.body.file
-            });
+            // Waiting for the promises
+            Promise.all(results).then(values => {
+                // Sending results
+                res.status(200).send({
+                    message: values
+                });
+            })
 
         } catch (error) {
-            console.log(err);
-            next(err);
-            res.status(503).send(err);
+            console.log(error);
+            next(error);
+            res.status(503).send(error);
             return;
         }
     } else {
