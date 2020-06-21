eed1e0be7324cbf997d22a3cfeab0dc65afc40b9
tataslas <tataslas@gmail.com>
2020-06-21 17:59:11 +0200

    [ID-0122] deleted console.log

--- project/src/components/QuestionnaireComponent/QuestionnaireComponent.vue
+++ project/src/components/QuestionnaireComponent/QuestionnaireComponent.vue
@@ -166,10 +166,10 @@ export default {
 
     },
     onStarted(stream) {
-       
+        console.log("On Started Event", stream);
     },
     onStopped(stream) {
-        
+        console.log("On Stopped Event", stream);
     },
     onStop() {
         this.$refs.webcam.stop();
@@ -178,16 +178,16 @@ export default {
         this.$refs.webcam.start();
     },
     onError(error) {
-       
+        console.log("On Error Event", error);
     },
     onCameras(cameras) {
         this.devices = cameras;
-        
+        console.log("On Cameras Event", cameras);
     },
     onCameraChange(deviceId) {
         this.deviceId = deviceId;
         this.camera = deviceId;
-        
+        console.log("On Camera Change Event", deviceId);
     }
   },
   mounted(){
